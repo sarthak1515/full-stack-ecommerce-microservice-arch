12ba90b4b46c9faf5f52b08a3ee5943df4dfd1f6
Sarthak Chaurasia <sarthakchaurasia16@gmail.com>
2025-11-09 12:43:34 +0530

    feat: integrated clerk auth in product service

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,12 +1,28 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import { clerkMiddleware, getAuth } from "@clerk/express";
 
 const app = express();
-app.use(cors({
-    origin: ["http://localhost:3002","http://localhost:3003"],
-    credentials: true
-}))
-app.get("/")
-app.listen(8000, ()=>{
-    console.log("product service is running")
-})
+app.use(
+  cors({
+    origin: ["http://localhost:3002", "http://localhost:3003"],
+    credentials: true,
+  })
+);
+app.use(clerkMiddleware());
+
+app.get("/test", (req, res) => {
+  const { isAuthenticated, userId } = getAuth(req);
+  console.log(userId);
+  if (!userId)
+    return res.status(401).json({
+      message: "You are not logged in!",
+    });
+
+  res.json({
+    message: "Product service authenticated",
+  });
+});
+app.listen(8000, () => {
+  console.log("Product service is running on port 8000");
+});
