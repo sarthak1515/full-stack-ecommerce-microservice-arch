036a31f9ad9283b87f7792814be71f5c988d1974
Sarthak Chaurasia <sarthakchaurasia16@gmail.com>
2025-11-09 13:00:46 +0530

    feat: integrated clerk auth in payment service

---
+++ apps/client/src/app/test/page.tsx
@@ -0,0 +1,15 @@
+import { auth } from "@clerk/nextjs/server";
+
+async function TestPage() {
+  const { getToken } = await auth();
+  const res = await fetch("http://127.0.0.1:8002/test", {
+    headers: {
+      Authorization: `Bearer ${await getToken()}`,
+    },
+  });
+  const data = await res.json();
+  console.log(data);
+  return <div>page</div>;
+}
+
+export default TestPage;
