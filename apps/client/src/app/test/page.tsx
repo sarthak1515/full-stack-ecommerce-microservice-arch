34b98bd60543403de7f00119f31d99ba84eebcaa
Sarthak Chaurasia <sarthakchaurasia16@gmail.com>
2025-11-09 13:01:09 +0530

    feat: Added test route on the client app

--- apps/client/src/app/test/page.tsx
+++ apps/client/src/app/test/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@clerk/nextjs/server";
 
+// Just for testing authentication
 async function TestPage() {
   const { getToken } = await auth();
   const res = await fetch("http://127.0.0.1:8002/test", {
