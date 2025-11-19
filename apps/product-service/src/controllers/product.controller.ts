158c4fc7499b4ef88214b330a6eb505e929f65bf
Sarthak Chaurasia <sarthakchaurasia16@gmail.com>
2025-11-19 17:59:07 +0530

    feat: completed updateProduct and deleteProduct controller

--- apps/product-service/src/controllers/product.controller.ts
+++ apps/product-service/src/controllers/product.controller.ts
@@ -26,8 +26,29 @@ export const createProduct = async (req: Request, res: Response) => {
   const product = await prisma.product.create({ data });
   res.status(201).json(product);
 };
-export const updateProduct = async (req: Request, res: Response) => {};
-export const deleteProduct = async (req: Request, res: Response) => {};
+
+export const updateProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data: Prisma.ProductUpdateInput = req.body;
+
+  const updatedProduct = await prisma.product.update({
+    where: { id: Number(id) },
+    data,
+  });
+
+  return res.status(200).json(updatedProduct);
+};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const deletedProduct = await prisma.product.delete({
+    where: { id: Number(id) },
+  });
+
+  return res.status(200).json(deletedProduct);
+};
+
 export const getProducts = async (req: Request, res: Response) => {
   const { sort, category, limit, search } = req.query;
 
