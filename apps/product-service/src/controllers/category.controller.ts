5fcd006929c3c6fc4fd63375478d7434d3ecedb7
Sarthak Chaurasia <sarthakchaurasia16@gmail.com>
2025-11-19 18:02:47 +0530

    feat: wrapped up category controller

--- apps/product-service/src/controllers/category.controller.ts
+++ apps/product-service/src/controllers/category.controller.ts
@@ -6,6 +6,28 @@ export const createCategory = async (req: Request, res: Response) => {
   const category = await prisma.category.create({ data });
   res.status(201).json(category);
 };
-export const updateCategory = async (req: Request, res: Response) => {};
-export const deleteCategory = async (req: Request, res: Response) => {};
-export const getCategories = async (req: Request, res: Response) => {};
+export const updateCategory = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data: Prisma.CategoryUpdateInput = req.body;
+
+  const category = await prisma.category.update({
+    where: { id: Number(id) },
+    data,
+  });
+
+  return res.status(200).json(category);
+};
+export const deleteCategory = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const category = await prisma.category.delete({
+    where: { id: Number(id) },
+  });
+
+  return res.status(200).json(category);
+};
+export const getCategories = async (req: Request, res: Response) => {
+  const categories = await prisma.category.findMany();
+
+  return res.status(200).json(categories);
+};
